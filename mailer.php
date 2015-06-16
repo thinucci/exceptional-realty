0f9f42084404b9cfa102c9ddf9ccd4aeb675006b
ticovfx <thiagonucci@gmail.com>
2015-06-15 20:37:34 -0400

    add php-mailer

--- mailer.php
+++ mailer.php
@@ -7,7 +7,7 @@
   ###################################
 
   // Set email address to send to i.e: you@yoursite.com
-  $sendTo       = 'you@yoursite.com';
+  $sendTo       = 'thiagonucci@gmail.com';
 
   // Set email subject (appears as subject)
   $subject      = 'Exceptional Realty - Contact Form Submission';
